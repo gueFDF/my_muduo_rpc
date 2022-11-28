592bdac92ed734b9b82c6a65f5d36587fb187fa6
gueFDF <3237455241@qq.com>
2022-11-28 16:12:44 +0800

    对getopt函数的使用

---
+++ test/getopt/main.cc
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    int ch;
+
+    printf("初始值：optind = %d，opterr = %d\n", optind, opterr);
+
+    while ((ch = getopt(argc, argv, "vha:b:c::")) != -1)
+    {
+        printf("ch = %c\n",(char)ch);
+        std::cout << ch << std::endl;
+        printf("开始解析参数：optind = %d\n", optind);
+        switch (ch)
+        {
+
+        case 'v':
+            printf("捕获到选项: -v\n");
+            printf("系统版本v1.1.0\n");
+            break;
+        case 'h':
+            printf("捕获到选项: -h\n");
+            printf("用法示例： -a 1 -b 2 -c [\"hello\"]\n");
+            break;
+        case 'a':
+            printf("捕获到选项: -a\n");
+            printf("该选项参数为：%s\n\n", optarg);
+            break;
+        case 'b':
+            printf("捕获到选项: -b\n");
+            printf("该选项参数为：%s\n\n", optarg);
+            break;
+        case 'c':
+            printf("捕获到选项: -c\n");
+            printf("该选项参数为：%s\n\n", optarg);
+            break;
+        }
+    }
+}
