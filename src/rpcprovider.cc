5524b12fb713a8a457d73842b325e9f370ede1cd
gueFDF <3237455241@qq.com>
2022-11-29 20:38:48 +0800

    添加buffer parse

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,6 +1,11 @@
 #include "rpcprovider.h"
 #include "mprpcappclication.h"
+#include "rpcheader.pb.h"
+#include <cstdint>
+#include <cstring>
 #include <google/protobuf/descriptor.h>
+#include <muduo/net/Endian.h>
+
 
 //发布rpc方法的函数接口
 void RpcProvider::NotifyService(google::protobuf::Service *service)
@@ -23,6 +28,9 @@ void RpcProvider::NotifyService(google::protobuf::Service *service)
   server_info.m_service = service;
   m_serviceMap.insert({server_name, server_info});
 }
+
+
+
 void RpcProvider::Run()
 {
     std::string ip = MprpcApplication::GetInitstance().GetConfig().Load("rpcserverip");
@@ -51,11 +59,44 @@ void RpcProvider::Run()
 
 void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr &conn)
 {
+  if(!conn->connected())
+  {
+    //连接断开
+    conn->shutdown();
+  }
 }
 
 void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr &conn,
                             muduo::net::Buffer *buffer,
                             muduo::Timestamp time)
 {
-    
+    //  //接受buffer字节流
+    std::string recv_buf=buffer->retrieveAllAsString();
+
+    //获取长度（需转换网络字节序）
+    uint32_t be32=0;
+    ::memcpy(&be32,recv_buf.c_str(),sizeof(be32));
+    uint32_t header_size=muduo::net::sockets::networkToHost32(be32);
+    //获取数据流
+    std::string rpc_header_str=recv_buf.substr(4,header_size);
+    mprpc::RpcHeader rpcHeader;
+    std::string service_name;
+    std::string method_name;
+    uint32_t args_size;
+    if(rpcHeader.ParseFromString(rpc_header_str))
+    {
+        //反序列化成功
+        service_name=rpcHeader.service_name();
+        method_name=rpcHeader.method_name();
+        args_size=rpcHeader.args_size();
+    }
+    else
+    {
+        //反序列化失败
+        std::cout<<"rpc_header_str is error : "<<rpc_header_str<<std::endl;
+        return;
+    }
+    //获取参数字节流
+    std::string args_str=recv_buf.substr(4+header_size,args_size);
+
 }
