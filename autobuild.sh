0f83ce4c3831ba5859c2d9c514a4d56f382d9670
gueFDF <3237455241@qq.com>
2022-12-04 18:16:12 +0800

    完善一键脚本

--- autobuild.sh
+++ autobuild.sh
@@ -1,3 +1,14 @@
+   # 重新生成proto文件
+   #src文件下
+   cd src
+   rm -rf ./include/rpcheader.pb.h  ./rpcheader.pb.c
+   protoc --cpp_out=./. rpcheader.proto 
+   mv rpcheader.pb.h  ./include
+   cd ..
+   #example目录下``
+   cd example
+   protoc --cpp_out=./   user.proto
+   cd ..
    cd build
    make clean
    cmake ..
