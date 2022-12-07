06c011b6b56c8f4506e76baf122e0a96601c843d
your name <your email>
2022-12-07 19:53:48 +0800

    rpcchannel

---
+++ src/include/mprpcchannel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <google/protobuf/service.h>
+class MprpcChannel : public google::protobuf::RpcChannel {
+  //所有通过stub代理对象调用的rpc方法，都走到这里了，统一做rpc方法调用的数据序列化和网络发发送
+  void CallMethod(const google::protobuf::MethodDescriptor *method,
+                  google::protobuf::RpcController *controller,
+                  const google::protobuf::Message *request,
+                  google::protobuf::Message *response,
+                  google::protobuf::Closure *done);
+};
---
+++ src/mprpcchannel.cc
@@ -0,0 +1,9 @@
+#include "mprpcchannel.h"
+
+void MprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
+                              google::protobuf::RpcController *controller,
+                              const google::protobuf::Message *request,
+                              google::protobuf::Message *response,
+                              google::protobuf::Closure *done) {
+  
+    }
